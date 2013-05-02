ba46c9887bd350ea67a23d93523d7d63ef15099e
Clebert Suconic <clebert.suconic@gmail.com>
2013-05-02 17:46:02 -0400

    Transactional

--- ejbs/src/main/java/com/redhat/mdb/ExampleMDB.java
+++ ejbs/src/main/java/com/redhat/mdb/ExampleMDB.java
@@ -19,11 +19,47 @@ import java.util.concurrent.atomic.AtomicInteger;
 @TransactionManagement(value = TransactionManagementType.CONTAINER)
 @TransactionAttribute(value = TransactionAttributeType.REQUIRED)
 public class ExampleMDB implements MessageListener {
-    public void onMessage(Message m) {
-        try {
-                        System.out.println("Message " + m.getStringProperty("count"));
-                } catch (JMSException e) {
-                        e.printStackTrace();
-                }
-    }
+
+   @Resource
+   private MessageDrivenContext context;
+
+   @Resource(mappedName = "java:/JmsXA")
+   private ConnectionFactory cf;
+
+   private Queue queue = null;
+
+   public void onMessage(Message m) {
+      Connection con = null;
+      Session session = null;
+      try {
+         Thread.sleep(500);
+         System.out.println("Message " + m.getStringProperty("count"));
+
+
+
+         con = cf.createConnection();
+
+         session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         if (queue == null)  {
+            queue = session.createQueue("OutQueue");
+         }
+         MessageProducer sender = session.createProducer(queue);
+         TextMessage newMessage = session.createTextMessage("received message from " + m.getStringProperty("count"));
+         newMessage.setStringProperty("inMessageId", m.getJMSMessageID());
+         sender.send(newMessage);
+
+      } catch (Exception t) {
+         t.printStackTrace();
+         this.context.setRollbackOnly();
+      } finally {
+         if (con != null) {
+            try {
+               con.close();
+            } catch (JMSException e) {
+               e.printStackTrace();
+            }
+         }
+      }
+   }
 }
